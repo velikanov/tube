a997d9a9aa43ebbee6bc34dda4985cfd18344b8a
velikan (home) <velikan55555@hotmail.com>
2017-10-27 00:41:07 +0300

    Initial Commit

---
+++ scheduler/src/main/java/com/company/scheduler/crawler/Crawler.java
@@ -0,0 +1,5 @@
+package com.company.scheduler.crawler;
+
+public interface Crawler {
+    void crawl();
+}
---
+++ scheduler/src/main/java/com/company/scheduler/crawler/VideoCrawler.java
@@ -0,0 +1,13 @@
+package com.company.scheduler.crawler;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class VideoCrawler implements Crawler {
+    @Override
+    public void crawl() {
+        System.out.println("New videos crawling started");
+
+        System.out.println("New videos crawling ended");
+    }
+}
